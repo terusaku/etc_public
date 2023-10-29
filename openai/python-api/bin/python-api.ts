b7d00c8a4380b7d710e9c79f1db84fa1683b531e
terusaku <sunblo117@gmail.com>
2023-10-30 01:19:20 +0900

    chore(python-api):

--- openai/python-api/bin/python-api.ts
+++ openai/python-api/bin/python-api.ts
@@ -1,21 +1,19 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
+
+import * as path from 'path';
+
 import * as cdk from 'aws-cdk-lib';
 import { PythonApiStack } from '../lib/python-api-stack';
 
+
+const currentDir = process.cwd();
+const projectName = path.basename(currentDir);
+
 const app = new cdk.App();
-new PythonApiStack(app, 'PythonApiStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+cdk.Tags.of(app).add('Project', 'openai-chatgpt');
+cdk.Tags.of(app).add('ManagedBy', `aws-cdk/${projectName}`);
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
+new PythonApiStack(app, 'ChatGptApiStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
